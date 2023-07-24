76356c39b50364d2786f7185c1fb1884d36a2f9d
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-07-24 21:04:21 +0300

    Creating Get and Post

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("ParseForm() error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, "Post Request success\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name - %s\n", name)
+	fmt.Fprintf(w, "Address - %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.NotFound(w, r)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprint(w, "hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Starting server at port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
